f7c6efe574bd0d024b7a5086aae074569083e04f
Vishwas Singh <singh.vishu234@gmail.com>
2020-04-03 15:58:26 +0530

    exercise_2

--- EX-2/ex2.1.py
+++ EX-2/ex2.1.py
@@ -1,43 +1,87 @@
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt  # To visualize
-import pandas as pd  # To read data
-from sklearn.linear_model import LogisticRegression
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('ex2data1.txt', delimiter=',', header = None)
+from scipy.optimize import minimize
 
-res = df.iloc[:,2].values
-exam_1 = df.iloc[:,0].values
-exam_2 = df.iloc[:,1].values
+from sklearn.preprocessing import PolynomialFeatures
 
-# X = feature values, all the columns except the last column
-X = df.iloc[:, :-1]
 
-# y = target values, last column of the data frame
-y = df.iloc[:, -1]
+def loaddata(file, delimeter):
+    data = np.loadtxt(file, delimiter=delimeter)
+    return(data)
 
-ex1_pos = []
-ex2_pos = []
-ex1_neg = []
-ex2_neg = []
+def plotData(data, label_x, label_y, label_pos, label_neg, axes=None):
+    # Get indexes for class 0 and class 1
+    neg = data[:,2] == 0
+    pos = data[:,2] == 1
+    
+    # If no specific axes object has been passed, get the current axes.
+    
+    plt.scatter(data[pos][:,0], data[pos][:,1], marker='+', c='k', s=60, linewidth=2, label=label_pos)
+    plt.scatter(data[neg][:,0], data[neg][:,1], color="yellow", s=60, label=label_neg)
+    plt.xlabel(label_x)
+    plt.ylabel(label_y)
+    plt.legend()
 
-for i in range(100):
-	if(res[i]==1):
-		ex1_pos.append(exam_1[i])
-		ex2_pos.append(exam_2[i])
-	else:
-		ex1_neg.append(exam_1[i])
-		ex2_neg.append(exam_2[i])
 
-plt.xlabel('Exam-1 score')
-plt.ylabel('Exam-2 score')
+data = loaddata('ex2data1.txt', ',')
 
-plt.scatter(ex1_neg,ex2_neg, marker="o", label="Not admiited")
-plt.scatter(ex1_pos,ex2_pos, marker = "+", label= "Admiited")
-plt.legend()
+X = np.c_[np.ones((data.shape[0],1)), data[:,0:2]]
+y = np.c_[data[:,2]]
 
-model = LogisticRegression()
-model.fit(X, y)
-y_pred = model.predict(X)
-plt.plot(X, y_pred, color='red')
+def sigmoid(z):
+    return(1 / (1 + np.exp(-z)))
 
-plt.show()
+
+def costFunction(theta, X, y):
+    m = y.size
+    h = sigmoid(X.dot(theta))
+    
+    J = -1*(1/m)*(np.log(h).T.dot(y)+np.log(1-h).T.dot(1-y))
+               
+    if np.isnan(J[0]):
+        return(np.inf)
+    return(J[0])
+
+   
+def gradient(theta, X, y):
+    m = y.size
+    h = sigmoid(X.dot(theta.reshape(-1,1)))
+    
+    grad =(1/m)*X.T.dot(h-y)
+
+    return(grad.flatten())
+
+initial_theta = np.zeros(X.shape[1])
+cost = costFunction(initial_theta, X, y)
+grad = gradient(initial_theta, X, y)
+print('Cost: \n', cost)
+print('Grad: \n', grad)
+
+
+res = minimize(costFunction, initial_theta, args=(X,y), method=None, jac=gradient, options={'maxiter':400})
+res
+
+def predict(theta, X, threshold=0.5):
+    p = sigmoid(X.dot(theta.T)) >= threshold
+    return(p.astype('int'))
+
+sigmoid(np.array([1, 45, 85]).dot(res.x.T))
+
+p = predict(res.x, X) 
+print('Train accuracy {}%'.format(100*sum(p == y.ravel())/p.size))
+            
+sc_1 = int(input("Enter exam1 score: "))
+sc_2 = int(input("Enter exam2 score: "))
+
+plt.scatter(sc_1, sc_2, s=60, c='r', marker='v', label=str(sc_1)+","+str(sc_2))
+plotData(data, 'Exam 1 score', 'Exam 2 score', 'Admitted', 'Not admitted')
+x1_min, x1_max = X[:,1].min(), X[:,1].max(),
+x2_min, x2_max = X[:,2].min(), X[:,2].max(),
+xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max), np.linspace(x2_min, x2_max))
+h = sigmoid(np.c_[np.ones((xx1.ravel().shape[0],1)), xx1.ravel(), xx2.ravel()].dot(res.x))
+h = h.reshape(xx1.shape)
+plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='b');      
+plt.show()      
